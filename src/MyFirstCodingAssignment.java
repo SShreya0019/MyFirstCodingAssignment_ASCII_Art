cd71808b612bd0c370e76cdd4e4d39f10b754fc5
100173778 <100173778@lubbockisd.net>
2023-09-05 09:16:38 -0500

    HELP ME

--- src/MyFirstCodingAssignment.java
+++ src/MyFirstCodingAssignment.java
@@ -1,5 +1,34 @@
-public class Main {
+public class MyFirstCodingAssignment {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\n" +
+                "                                ^\n" +
+                "                             (     )\n" +
+                "                           {         }\n" +
+                "                            (       )\n" +
+                "\n" +
+                "                   ///////////  ! /////////////\n" +
+                "             ////////                      /////////     \n" +
+                "             /     ///////////////////////////     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "             /                                     /\n" +
+                "                //   ///////////////////////// //                             \n");
     }
 }
